99add185cb5c392f965b17d4652f288efea35c0e
Tom Simula <tom.simul@gmail.com>
2022-02-02 17:09:52 +0100

    Le b&b doit normalement (a prendre avec des pincettes) fonctionner

--- bb.go
+++ bb.go
@@ -8,14 +8,11 @@ import (
 func main() {
 	var tableau = [][]float64{{1,1}, {2,-1}, {-1,2}}
 	var tabConst = []float64{2,0,1}
-
-	branch_bound(simplexe(tableau, tabConst), tableau, tabConst)
+	channel := make(chan []float64)
+	branch_bound(simplexe(tableau, tabConst), tableau, tabConst, channel)
 }
 
-func branch_bound(solution []float64, gotSol bool, tableau [][]float64, tabConst []float64) []flaot64{
-
-	//TODO: Cas d'arret si on a pas de solution
-	//Comment savoir qu'on a pas de solution ?
+func branch_bound(solution []float64, gotSol bool, tableau [][]float64, tabConst []float64, channel chan []float64) []flaot64{
 
 	//Cas d'arret si solution est fait seulement d'entier
 	if(estSol(solution)){
@@ -24,28 +21,33 @@ func branch_bound(solution []float64, gotSol bool, tableau [][]float64, tabConst
 	for index, element := range solution {
 		if(!isInteger(element)){
 			for i := 0; i < 2; i++ {
-				var tableauBis []float64
-				var tabConstBis []float64
+				go func() {
+					var tableauBis []float64
+					var tabConstBis []float64
+					channelBis := make(chan []float64)
 
-				//Copie de tableau et du tableau de contrainte
-				for i := 0; i < len(tabConst); i++ {
-					tabConstBis = append(tab, tabConst[i])
-				}
-				for i := 0; i < len(tableau); i++ {
-					tableauBis = append(tab, tableau[i])
-				}
+					//Copie de tableau et du tableau de contrainte
+					for j := 0; j < len(tabConst); j++ {
+						tabConstBis = append(tabConstBis, tabConst[i])
+					}
+					for j := 0; j < len(tableau); j++ {
+						tableauBis = append(tableauBis, tableau[i])
+					}
 
-				//Ajout de la nouvelle contrainte dans les copies de tableau
-				if i==0 {
-					//Traduction tableau et contrainte de Margaux
-				} else {
-					//Traduction tableau et contrainte de Margaux
-				}
+					//Ajout de la nouvelle contrainte dans les copies de tableau
+					if i==0 {
+						//Traduction tableau et contrainte de Margaux
+					} else {
+						//Traduction tableau et contrainte de Margaux
+					}
 
-				return go branch_bound(simplexe(tableauBis, tabConstBis), tableauBis, tabConstBis)
+					channel <- branch_bound(simplexe(tableauBis, tabConstBis), tableauBis, tabConstBis, channelBis)
+				}
 			}
 		}	
-	}	
+	}
+	sol := <- channel
+	return sol	
 }
 
 //Verifie que le nombre donné soit un entier
