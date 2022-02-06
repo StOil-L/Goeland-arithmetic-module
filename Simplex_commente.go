31f962945e274331d67df8ecafc88dd110ee4abc
cedric <cedric.cahuzac@etu.umontpellier.fr>
2022-02-06 18:52:16 +0100

    simplex avec variable bien nommées

--- Simplex_commente.go
+++ Simplex_commente.go
@@ -1,50 +1,56 @@
 package main
 
+
 import (
 	"fmt"
 	"strconv"
+    "strings"
+    "regexp"
+	"bufio"
+    "os"
 //	"math/big"
-
 //	"time"
+    
 )
 
 
 func main() {
-	fmt.Println("choisissez le test que vous voulez executer : \n 1 pour : x+y>=2,2x-y>=0,-x+2y>=1 \n 2 pour : x+y>=0,x+y>=1,x+y>=2,x+y>=3,x+y>=4 \n 3 pour : x+y>=0,x+2y>=1,x+3y>=2,x+4y>=3,x+5y>=4 \n 4 pour : x>=1/4,x<=1/5 \n 5 pour : x=1/4 \n 6 pour : construire votre matrice des coefficients et vos contraintes")
+	fmt.Println("choisissez le test que vous voulez executer : \n 1 pour : x+y>=2,2x-y>=0,-x+2y>=1 \n 2 pour : x+y>=0,x+y>=1,x+y>=2,x+y>=3,x+y>=4 \n 3 pour : x+y>=0,x+2y>=1,x+3y>=2,x+4y>=3,x+5y>=4 \n 4 pour : x>=1/4,x<=1/5 \n 5 pour : x=1/4 \n 6 pour : construire votre matrice des coefficients et vos contraintes \n 7 pour : faire appel au parseur")
 	var x int
 	fmt.Scanln(&x)
+	var tabVar = make([]string,0)
 	if x==1 {
 		var tableau = [][]float64{{1,1}, {2,-1}, {-1,2}}
 		var tabConst = []float64{2,0,1}
 		fmt.Println("x+y>=2,2x-y>=0,-x+2y>=1")
-		fmt.Println(simplex(tableau, tabConst))
+		fmt.Println(simplex(tableau, tabConst,tabVar))
 	}
 	if x==2{
 
 		var tableau = [][]float64{{1,1}, {1,1}, {1,1}, {1,1}, {1,1}}
 		var tabConst = []float64{0,1,2,3,4}
 		fmt.Println("x+y>=0,x+y>=1,x+y>=2,x+y>=3,x+y>=4")
-		fmt.Println(simplex(tableau,tabConst))
+		fmt.Println(simplex(tableau,tabConst,tabVar))
 
 	}
 	if x==3{
 		var tableau = [][]float64{{1,1}, {1,2}, {1,3}, {1,4}, {1,5}}
 		var tabConst = []float64{0,1,2,3,4}
 		fmt.Println("x+y>=0,x+2y>=1,x+3y>=2,x+4y>=3,x+5y>=4")
-		fmt.Println(simplex(tableau,tabConst))
+		fmt.Println(simplex(tableau,tabConst,tabVar))
 	}
 	if x==4{
 		var tableau = [][]float64{{1}, {-1}}
 		var tabConst = []float64{0.25,-0.2}
 		fmt.Println("x>=1/4,x<=1/5")
-		fmt.Println(simplex(tableau,tabConst))
+		fmt.Println(simplex(tableau,tabConst,tabVar))
 	}
 
 	if x==5{
 		var tableau = [][]float64{{1}, {-1}}
 		var tabConst = []float64{0.25,-0.25}
 		fmt.Println("x=1/4")
-		fmt.Println(simplex(tableau,tabConst))
+		fmt.Println(simplex(tableau,tabConst,tabVar))
 	}
 
 	if x==6{
@@ -87,19 +93,47 @@ func main() {
 		}
 
 		fmt.Println(tableau, tabConst)
-		fmt.Println(simplex(tableau, tabConst))
+		fmt.Println(simplex(tableau, tabConst, tabVar))
 		
 	}
 	fmt.Println("\033[0m") 
+	
+	if x == 7 {
+	    // Creation du tableau de coeff et du tableau de contraintes
+        var tableau = make([][]float64,0) 
+        var tabConst = make([]float64,0)
+        var tabVar = make([]string,0)
+        fmt.Println("Veuillez saisir le nombre d'equations")
+        var nbrEq int
+        fmt.Scanln(&nbrEq)
+        fmt.Println("nbrEq =",nbrEq)
+    	var tabExe = make([]string,0)
+    	//var equation string
+    	for i:=0; i<nbrEq; i++{
+    	    reader := bufio.NewReader(os.Stdin)
+            fmt.Print("Entrez une equation: ")
+            equation, _ := reader.ReadString('\n')
+    		tabExe = append(tabExe, equation)
+    	}
+    	
+    	fmt.Println("tabExe =",tabExe)
+        //var tabExe = []string{"20 t - x + y -18 z >= 8","0 t -5 x + y -0 z >= 5","-7 t +3 x +5 y + z >= 33"}
+        
+        tabConst, tableau, tabVar = addAllConst(tabExe, tableau, tabConst, tabVar)
+        fmt.Println("tableau = ",tableau)
+        fmt.Println("tabConst = ",tabConst)
+        fmt.Println("tabVar = ",tabVar)
+        fmt.Println(simplex(tableau, tabConst, tabVar))
+	}
 
 }
 //donnees: le "Tableau" des coeffs et un tableau contenant les contraintes
 //retour : solution s'il y en a une, sinon nil 
-func simplex(tableau [][]float64, tabConst []float64) (map[string]float64, bool){
+func simplex(tableau [][]float64, tabConst []float64, tabVar[]string) (map[string]float64, bool){
 	//creation tableau des affectations : taille = nombre de ligne + nombre de colonnes
-	alphaTab := createAlphaTab(tableau)
+	alphaTab := createAlphaTab(tableau, tabVar)
 	//tableau qui nous donne la postion des variables dans le tableau alphaTab
-	var posVarTableau = createPosVarTableau(tableau)
+	var posVarTableau = createPosVarTableau(tableau, tabVar)
 	var bland = make([]string, len(posVarTableau))
 	
 	for i:=0;i<len(tableau);i++ {
@@ -225,28 +259,39 @@ func pivot(tableau [][]float64,  tabConst []float64,
 
 
 //creation d'un dictionnaire (clé:nomDesVariable et valeur:affectation)
-func createAlphaTab(tableau [][]float64) map[string]float64{
+func createAlphaTab(tableau [][]float64, tabVar []string) map[string]float64{
 	alphaTab := make(map[string]float64)
 	for i := 0; i < len(tableau); i++ {
 		alphaTab[fmt.Sprint("e", i)] = 0
 	}
-	for i := 0; i < len(tableau[0]); i++ {
+	if len(tabVar) == 0 {
+	    for i := 0; i < len(tableau[0]); i++ {
 		alphaTab[fmt.Sprint("v", i)] = 0
-	}
+	    }
+    } else {
+        for i := 0; i < len(tableau[0]); i++ {
+            alphaTab[tabVar[i]] = 0
+	    }
+    }
 	return alphaTab
 }
 
 
-func createPosVarTableau(tableau [][]float64) []string{
+func createPosVarTableau(tableau [][]float64, tabVar[]string) []string{
 	var posVarTableau = make([]string, len(tableau[0])+len(tableau))
 	lenTab := len(tableau)
 	for i := 0; i < lenTab + len(tableau[0]); i++ {
 		if i < lenTab {
 			posVarTableau[i] = fmt.Sprint("e", i)
-		} else {   //margaux, dans ce else il faudra remplacer fmt.sprint par tabVar[i-lenTab]
-			posVarTableau[i] = fmt.Sprint("v", i - lenTab)
+		} else {
+		    if len(tabVar) == 0 {
+		        posVarTableau[i] = fmt.Sprint("v", i - lenTab)
+		    } else {
+		        posVarTableau[i] = tabVar[i-lenTab]
+		    }
 		}
 	}
+	
 	return posVarTableau
 }
 
@@ -320,3 +365,79 @@ func coefficients(tableau [][]float64, columnPivot int, workingLine int, Increme
 	}
 	
 }
+
+// fonction qui prend en parametre un tableau d'equation eqs, une matrice de coeff et un tableau de contraintes 
+// et renvoit ces tableaux de coeff et de contraintes remplis
+func addAllConst(eqs []string, tableau [][]float64, tabConst []float64, tabVar[]string) ([]float64, [][]float64, []string){
+    for _, element := range eqs {
+        lastEle, tab, Var := addOneConst(element)
+        tableau = append(tableau, tab)
+        tabConst = append(tabConst, lastEle)
+
+        for i := 0;i < len(Var);i++{
+          var present bool
+          present = false
+          for j := 0;j < len(tabVar);j++{
+            if Var[i] == tabVar[j] {
+              present = true
+            }
+          }
+          if present == false {
+              tabVar = append(tabVar,Var[i])
+            }
+        }
+        //fmt.Println("tabConst =",tabConst)
+    }
+    return tabConst, tableau, tabVar
+}
+
+// fonction qui prend en parametre une equation et qui implemente les tableaux de coeff et de contraintes 
+func addOneConst(eq string) (float64, []float64,[]string){
+    // on split la string avec les espaces, ce qui nous donne un tableaux avec tous les elements de la string
+    tabEle := strings.Split(eq, " ")
+    // tableau qui va contenir les coefficients de l'equation
+    var ligneEq []float64
+    var TabVar []string
+    // indique notre position dans le tableau
+    posTab := 0
+    for i := 0; i < len(tabEle)-2; i++ {
+        ligneEq = append(ligneEq, 1.0)
+        // nous permet de savoir si notre caractere est un chiffre
+        re := regexp.MustCompile(`[0-9]`)
+        isFig := re.FindString(tabEle[i])
+        // nous permet de savoir si notre caractere est une lettre
+        re2 := regexp.MustCompile(`[a-z]`)
+        isLet := re2.FindString(tabEle[i])
+
+        if isLet != "" {
+          TabVar = append(TabVar,isLet)
+        }       
+
+
+        // si on a un chiffre
+        if isFig != "" {
+            // on converti notre caractere qui est une string en float64
+            conv,_ := strconv.ParseFloat(tabEle[i], 64)
+            // on l'insere dans notre tableau a la position "posTab"
+            ligneEq[posTab] = conv
+
+            
+        } else {
+            // le cas du -
+            if tabEle[i] == "-"{
+                ligneEq[posTab] = -1.0
+            // le cas du +
+            } else if tabEle[i] != "+" {
+                posTab += 1
+            }
+        }
+    }
+
+    // On ajoute la contraintes dans le tableau de contraintes en l'a convertissant d'abord
+    lastEle := tabEle[len(tabEle)-1]
+    lastEleC,_ := strconv.ParseFloat(lastEle, 64)
+    fmt.Println("lastEleC =",lastEleC)
+    
+    return lastEleC, ligneEq[0:posTab],TabVar
+}
+
